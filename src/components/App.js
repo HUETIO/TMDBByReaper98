1a80bbf2bc3aaa9753ff5fb48935cae4bbbe13fd
Diego Alejandro Sepulveda Huetio <alhejhosk8nxp@gmail.com>
2024-07-12 16:42:21 -0500

    actualizacion
    actualizacion

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,62 @@
-import { render } from '@testing-library/react';
-import './App.css';
-import Nav from './Nav'
+import React, { useState } from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import Nav from './Nav';
 import SearchArea from './SearchArea';
 import MovieList from './MovieList';
-import { Component } from 'react';
+import './App.css';
 
-class App extends Component{
-  constructor (){
-    super()
-    this.state = {
-      movies:[],
-      searchTerm: ''
-    }
-    this.apiKey = process.env.REACT_APP_API
-  }
+const App = () => {
+  const apiKey = process.env.REACT_APP_API;
 
-  handleSubmit =(e) =>{
+  const [searchTerm, setSearchTerm] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.state.searchTerm}`)
-    .then (data => data.json())
-    .then (data => {
-      console.log(data);
-      this.setState({movies:[...data.results]})
-    })
-  }
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchTerm}`)
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data);
+        setMovies([...data.results]);
+      });
+  };
 
-  handleChange =(e)=>{
-    this.setState({searchTerm: e.target.value})
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
-  }
+  const [value, setValue] = useState(0);
+  const images = [
+    'https://source.unsplash.com/1600x900/?nature,water',
+    'https://source.unsplash.com/1600x900/?nature,trees',
+    'https://source.unsplash.com/1600x900/?nature,mountains'
+  ];
 
-render (){
   return (
     <div className="App">
       <h1> ↓ Hola Bienvenid@s ↓ </h1>
-      <Nav/>
-      <SearchArea handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
-      <MovieList movies = {this.state.movies}/>
+      <Nav />
+      <SearchArea handleSubmit={handleSubmit} handleChange={handleChange} />
+      <MovieList movies={movies} />
+
+      <h1>slider prueba de css styles</h1>
+      <div className="slider-container">
+        <div className="slider">
+          <p>Valor: {value}</p>
+          <Slider
+            value={value}
+            onChange={setValue}
+            min={0}
+            max={images.length - 1}
+          />
+        </div>
+        <img src={images[value]} alt="Slider" className="slider-image" />
+      </div>
+
       <h6> → Prueba echa por Diego Sepulveda H. By Reaper98 </h6>
-      
     </div>
-  )
-}
-
-}
-
-
-
+  );
+};
 
 export default App;
