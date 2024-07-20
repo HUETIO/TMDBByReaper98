da34b3f90108e567b00c6b9d6e2dfd8bb59cf665
Diego Alejandro Sepulveda Huetio <alhejhosk8nxp@gmail.com>
2024-07-20 12:28:21 -0500

    final versions
    update of program, and styles.css because the program are failure colors

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,1 +1,16 @@
-import React
+import React from "react";
+import SearchArea from "./SearchArea";
+
+const Nav = ({ SearchArea }) => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+
+                <h1 className="banner"> TMDB </h1>
+                {SearchArea}
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
